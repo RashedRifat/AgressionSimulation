29d25ead61944e70bb8c9bb07c8bd0392def0502
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2021-05-27 22:37:17 -0400

    Update forestry.py
    add distribute methods and memory management

--- agressionSimulation/forestry.py
+++ agressionSimulation/forestry.py
@@ -8,6 +8,7 @@ except:
 NoneType = type(None)
 
 class clearing():
+    
     def __init__(self, calories, growthMean, growthSD, consumePerMeal=0.7, simulation_logger=None):
         self.calories = calories
         self.growthMean = growthMean
@@ -45,6 +46,10 @@ class clearing():
 
         return self.birds[0].procreate(self.birds[1])
     
+    def reset(self):
+        del self.birds
+        self.birds = []
+    
     def time(self):
         # A day passes in the clearing
         # This function returns the new children, if generated 
@@ -58,40 +63,81 @@ class clearing():
 
         # Resolve aggression, procreation and clearing growth. Log the child (if there is a child). 
         children = self.resolve()
-        for bird in children:
+        if children:
             self.simulation_logger.add(bird, child=True)
         self.growth()
 
         return children
 
 class forest():
-    def __init__(self, totalSpaces, calorieMean, calorieSD, growthMean, growthSD, simulation_logger=None):
+
+    def __init__(self, totalSpaces, calorieMean, calorieSD, growthMean, growthSD, maxSearch=3, simulation_logger=None):
         self.totalSpaces = totalSpaces
         self.spaces = []
         self.allBirds = []
         self.nonOccupiedBirds = []
+        self.maxSearch = maxSearch
+        self.calorieMean = calorieMean
+        self.calorieSD = calorieSD
+        self.growthMean = growthMean
+        self.growthSD = growthSD
         self.simulation_logger = simulation_logger
     
     def load(self, allBirds):
         self.allBirds = allBirds
-    
-    def occupy(self):
+
         # Create all spaces  
         for i in range(0, self.totalSpaces):
-            self.spaces.append(clearing())           
-        
-        # Distribute all birds across the spaces 
-        # Birds that did not find a clearing should be added to the nonOccupiedBirds list
+            calories = int(np.random.normal(loc=self.calorieMean, scale=self.calorieSD, size=1))
+            self.spaces.append(clearing(calories, self.growthMean, self.growthSD, simulation_logger=self.simulation_logger))           
+    
+    def occupy(self):
+        # Shuffle all the birds 
         random.shuffle(self.allBirds)
-        spacesCounter = 0
+        occupiedBirdCounter = 0
         birdCounter = 0
 
+        # Iterate through the birds and attempt to add them to a clearing 
+        # If they fail more than the maxSearch times, add to nonccupied list 
+        for bird in self.allBirds:
+            maxSearchCounter = 0
 
-        return 0
+            # If the number of birds added is twice more than the number of spaces, the bird cannot be added 
+            if birdCounter > self.totalSpaces * 2:
+                self.nonOccupiedBirds.extend(self.allBirds[birdCounter+1:])
+                break
+
+            # The bird goes on the hunt and tries to occupy a clearing 
+            while maxSearchCounter < self.maxSearch:
+                maxSearchCounter += 1
+                randomSpace = random.randint(0, self.totalSpaces)
+                try:
+                    self.spaces[randomSpace].occupy(bird)
+                except:
+                    continue 
+
+            # If they failed more than they could, add them to the non-allocated list 
+            if maxSearchCounter > self.maxSearch:
+                self.nonOccupiedBirds.append(bird)
+            
+            birdCounter += 1
+            
 
     def time(self):
-        # Should distribute all of the allBirds array into the clearings 
-        # Should call the time function on all environments, currently supporting only the forest 
-        # Finally, update the allBirds array to reflect the changes (birth/death) of these birds 
 
-        return 0
+        # Should call the time function on all clearings, adding new birds to the total birds list 
+        for clring in self.spaces:
+            possibleChild = clring.time()
+            if possibleChild is not None:
+                self.allBirds.extend(possibleChild)
+
+            clring.reset()
+
+        # Finally, process all the birds in the non-occupied list 
+        for bird in self.nonOccupiedBirds:
+            bird.time()
+            self.simulation_logger.add(bird)
+        
+        # Reset the list, cleaning up memory as we go
+        del self.nonOccupiedBirds
+        self.nonOccupiedBirds = []
